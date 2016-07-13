454c3dfd17a1406663975a766f824dfefaa57336
Cesar De La Mora <cdelamoraalvarez@gmail.com>
2016-07-13 19:57:25 +0000

    Tarea 1
    Signed-off-by: Cesar De La Mora <cdelamoraalvarez@gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,18 +1,81 @@
-#!/usr/bin/python
-
+import paho.mqtt.client as paho
+import psutil
 import signal
 import sys
 import time
 
-def functionSignalHandler(signal, frame) :
+from threading import Thread
+
+DeviceID = 0
+
+def functionDataActuator(status):
+    print "Data Actuator Status %s" % status
+
+def functionDataActuatorMqttOnMessage(mosq, obj, msg):
+    print "Data Sensor Mqtt Subscribe Message!"
+    functionDataActuator(msg.payload)
+
+def functionDataActuatorMqttSubscribe():
+    STR = "IoT101/" + str(DeviceID) + "DataActuator"
+    mqttclient = paho.Client()
+    mqttclient.on_message = functionDataActuatorMqttOnMessage
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    mqttclient.subscribe(STR, 0)
+    while mqttclient.loop() == 0:
+        pass
+
+def functionDataSensor():
+    netdata = psutil.net_io_counters()
+    data = netdata.packets_sent + netdata.packets_recv
+    return data
+
+def functionDataSensorMqttOnPublish(mosq, obj, msg):
+    print "Data Sensor Mqtt Published!"
+
+def functionDataSensorMqttPublish():
+    STR = "IoT101/" + str(DeviceID) + "DataActuator"
+    mqttclient = paho.Client()
+    mqttclient.on_publish = functionDataSensorMqttOnPublish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    mqttclient.subscribe(STR, 0)
+    while mqttclient.loop() == 0:
+        pass
+
+def functionDataSensor():
+    netdata = psutil.net_io_counters()
+    data = netdata.packets_sent + netdata.packets_recv
+    return data
+
+def functionDataSensorMqttOnPublish(mosq, obj, msg):
+    print "Data Sensor Mqtt Published!"
+
+def functionDataSensorMqttPublish():
+    mqttclient = paho.Client()
+    mqttclient.on_publish = functionDataSensorMqttOnPublish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    while True:
+        data = functionDataSensor()
+        topic = "IoT101/DataSensor"
+        mqttclient.publish(topic, data)
+        time.sleep(1)
+
+def functionSignalHandler(signal, frame):
     sys.exit(0)
 
 if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, functionSignalHandler)
 
+    threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
+    threadmqttpublish.start()
+
+    threadmqttsubscribe = Thread(target=functionDataActuatorMqttSubscribe)
+    threadmqttsubscribe.start()
+
     while True:
         print "Hello Internet of Things 101"
+        print "Data Sensor: %s " % functionDataSensor()
         time.sleep(5)
 
 # End of File
+
